4eace6b727bb554d8faaba57ebf63c4f51554cd9
Matvey Demidov <96277549+screenviolence@users.noreply.github.com>
2025-09-25 23:49:59 +0700

    Update CORS settings for SSL redirection
    Removed conditional SSL redirect setting.

--- CoachDiary_Backend/settings/cors.py
+++ CoachDiary_Backend/settings/cors.py
@@ -1,8 +1,5 @@
 from django.conf import settings
 
 CORS_ALLOWED_ORIGINS = ["https://coachdiary.ru", "http://127.0.0.1:8000"]
+CSRF_TRUSTED_ORIGINS = ["https://coachdiary.ru", "http://127.0.0.1:8000"]
 CORS_ALLOW_CREDENTIALS = True
-SECURE_SSL_REDIRECT = False
-
-if not settings.DEBUG:
-    SECURE_SSL_REDIRECT = True
